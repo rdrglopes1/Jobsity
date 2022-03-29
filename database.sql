c138692f0b6751f6527621b6776e4b717ffdfb84
Rodrigo <rdrglopes1@gmail.com>
2022-03-29 17:35:02 -0300

    add database DDL

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE testDB;
+
+CREATE TABLE public.trips (
+	region varchar NULL,
+	origin_cord varchar NULL,
+	destination_cord varchar NULL,
+	datetime timestamp NULL,
+	datasource varchar NULL
+);
+
